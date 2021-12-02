45669ee9393f28366c6f6af683858a74886c0c36
Timofey Yatskov <90448236+cringecompiler@users.noreply.github.com>
2021-12-02 16:07:58 +0300

    Update test_5.py

--- issue-5/test_5.py
+++ issue-5/test_5.py
@@ -1,13 +1,27 @@
-from what_is_year_now import what_is_year_now
+import io
+from unittest.mock import patch
+
 import pytest
 
+from what_is_year_now import what_is_year_now
+import urllib.request
 
-def test_norm():
-    actual = what_is_year_now()
-    exp = 2021
-    assert actual == exp
+
+def test_ymd():
+    with patch.object(urllib.request, 'urlopen', return_value=io.StringIO('{"currentDateTime": "2019-03-01"}')):
+        actual = what_is_year_now()
+        exp = 2019
+        assert actual == exp
+
+
+def test_dmy():
+    with patch.object(urllib.request, 'urlopen', return_value=io.StringIO('{"currentDateTime": "01.03.2019"}')):
+        actual = what_is_year_now()
+        exp = 2019
+        assert actual == exp
 
 
 def test_ex():
-    with pytest.raises(Exception):
-        what_is_year_now(2)
+    with patch.object(urllib.request, 'urlopen', return_value=io.StringIO('{"currentDateTime": "20190303"}')):
+        with pytest.raises(ValueError):
+            what_is_year_now()
